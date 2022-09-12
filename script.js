02b10e4c287399e3f9eac3b43199a666507b215d
Alexis <alexischecura@gmail.com>
2022-09-12 17:44:11 -0300

    basic opeations

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 
 
-const add = (a,b) => a + b
-const subtract = (a,b) => a - b
-const multyply = (a,b) => a * b
-const divide = (a,b) => a / b
+const add = (a,b) => a + b;
+const subtract = (a,b) => a - b;
+const multyply = (a,b) => a * b;
+const divide = (a,b) => a / b;
+
+const operate = (operator, a, b) => {
+    switch (operator) {
+        case "+":
+            return add(a,b)
+        case "-":
+            return subtract(a,b)
+        case "*":
+            return multyply(a,b)
+        case "/":
+            return divide(a,b)
+        default:
+            return "Operation not found"
+    }
+}
