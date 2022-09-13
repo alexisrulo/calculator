d2678d6d115a47ffaafa14dde5702381565671e5
Alexis <alexischecura@gmail.com>
2022-09-13 11:20:12 -0300

    Working well

--- script.js
+++ script.js
@@ -3,7 +3,9 @@
 const add = (a, b) => Number(a) + Number(b);
 const subtract = (a, b) => Number(a) - Number(b);
 const multyply = (a, b) => Number(a) * Number(b);
-const divide = (a, b) => Number(a) / Number(b);
+const divide = (a, b) => {
+    if(b==="0") return "Math Error"
+    return Number(a) / Number(b)};
 const percent = (a, b) => (Number(a) * Number(b)) / 100;
 
 const operate = (operator, a, b) => {
@@ -28,10 +30,8 @@ const downNumber = document.querySelector(".downNumber");
 const upNumber = document.querySelector(".upNumber");
 
 const numbers = document.querySelectorAll(".number");
-let num1 = '';
+let num1 = "";
 let num2;
-let numero1 = 1;
-let numero2 = 0;
 let operation;
 numbers.forEach((num) =>
   num.addEventListener("click", () => {
@@ -44,19 +44,21 @@ const operators = document.querySelectorAll(".operator");
 
 operators.forEach((op) =>
   op.addEventListener("click", () => {
-    if (num2 != undefined) {
+    if (num2 != undefined && num1) {
       let result = operate(operation, num2, num1);
       downNumber.innerText = result;
       upNumber.innerText = result + op.innerHTML;
       operation = op.innerHTML;
       num2 = result;
-      num1 = '';
-    } 
-    else {
+      num1 = "";
+    } else if (num1 === "") {
+      operation = op.innerHTML;
+      upNumber.innerText = num2 + operation;
+    } else {
       operation = op.innerHTML;
       num2 = num1;
       upNumber.innerText = num1 + operation;
-      num1 = '';
+      num1 = "";
     }
   })
 );
@@ -68,7 +70,9 @@ igual.addEventListener("click", () => {
     downNumber.innerText = result;
     upNumber.innerText = num2 + operation + num1 + '=';
     num2 = result;
-    num1 = '';
+    num1 = "";
+  } else if (num1 === '') {
+    upNumber.innerText = num2 + '=';
   } else {
     upNumber.innerText = num1 + '=';
   }
@@ -79,3 +83,23 @@ igual.addEventListener("click", () => {
 changeSimbol = document.querySelector(".changeSimbol");
 
 changeSimbol.addEventListener("click", () => {});
+
+//Reset
+
+const ac = document.querySelector('.ac')
+
+ac.addEventListener('click', () =>{
+    num1 = '';
+    num2 = '';
+    operation = '';
+    upNumber.innerText = '';
+    downNumber.innerText = '0';
+  })
+
+const reset = () =>{
+  num1 = '';
+  num2 = '';
+  operation = '';
+  upNumber.innerText = '';
+  downNumber.innerText = '0';
+}
